77f40da833494f756a2a6d1bec99e1a285c296d9
Kyler Hamilton <tech@kyler.me>
2014-10-24 14:41:48 -0400

    Initial Commit

---
+++ js/action.js
@@ -0,0 +1,29 @@
+ $(function() {
+
+        $('#action_form').bind('submit', function(e){
+            var form = $(this);
+            var buttons = form.find('button[type=submit]');
+
+
+            buttons.attr('disabled', 'disabled').text('Working...');
+            
+            $.post(form.attr('action'), form.serialize(), function(data){
+                $action_widget = document.getElementById('action_widget');
+                $action_widget.className = ( $action_widget.className == 'share-widget' )? '' : 'share-widget';
+            });
+
+            e.preventDefault(); 
+        }); 
+        
+
+});
+
+
+$(document).ready(function() {
+     $('button[type="submit"]').attr('disabled','disabled');
+     $('input[type="text"]').keyup(function() {
+        if( $('#input-name').val() != '' && $('#input-email').val().indexOf('@') >= 0)  {
+           $('button[type="submit"]').removeAttr('disabled');
+          }
+       });
+     });
